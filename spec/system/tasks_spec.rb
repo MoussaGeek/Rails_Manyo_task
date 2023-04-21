1850af3101a453f505c0d86b059c1406b3514735
MoussaGeek <moussadiakite1963@gmail.com>
2023-04-21 13:35:23 +0000

    commitstep2

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -12,11 +12,28 @@ RSpec.describe 'Task management function', type: :system do
   end
 
   describe 'List display function' do
+    let!(:first_task) { FactoryBot.create(:task, created_at: '2025-02-18') }
+    let!(:second_task) { FactoryBot.create(:task, created_at: '2025-02-17') }
+    let!(:third_task) { FactoryBot.create(:task, created_at: '2025-02-16') }
+
+    before do
+      visit tasks_path
+    end
+
     context 'When transitioning to the list screen' do
-      it 'A list of registered tasks is displayed' do
-        FactoryBot.create(:task)
+      it 'The list of created tasks is displayed in descending order of creation date and time.' do
+        task_list = all('tbody tr')
+      end
+    end
+    context 'When creating a new task' do
+      let!(:new_task) { FactoryBot.create(:task, title: 'new_task_title', created_at: '2025-02-19') }
+
+      before do
         visit tasks_path
-        expect(page).to have_content 'Préparation des documents'
+      end
+      it 'New task is displayed at the top' do
+        task_list = all('tbody tr')
+        expect(task_list[0]).to have_content('new_task_title')
       end
     end
   end
