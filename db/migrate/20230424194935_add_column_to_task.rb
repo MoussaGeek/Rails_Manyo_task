72270ca4503ec6e4796b78e5fc3f9133a983738d
MoussaGeek <moussadiakite1963@gmail.com>
2023-04-27 09:58:47 +0000

    firstcomit

--- db/migrate/20230424194935_add_column_to_task.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnToTask < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tasks, :deadline_on, :date, null: false
-    add_column :tasks, :priority, :integer, null: false
-    add_column :tasks, :status, :integer, null: false
-  end
-end
